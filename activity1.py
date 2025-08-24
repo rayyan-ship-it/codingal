bbaba8867f5259aa63c6fbb6742b5e00fcd26208
rayyan-ship-it <rayyanop383@gmail.com>
2025-08-24 18:43:51 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,17 @@
+x=7.0
+if type(x) is float:
+    print("this is true")
+else:
+    print("this is false")
+a=13
+if type(a) is not float:
+    print("true")
+else:
+    print("false")
+
+d=50
+e=30
+if type(d and e) is int:
+    print("both are integers")
+else:
+    print("both are floats")

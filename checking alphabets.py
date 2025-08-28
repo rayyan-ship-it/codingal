6d7c0316a29b2f43930984ada596f5bbe8f63580
rayyan-ship-it <rayyanop383@gmail.com>
2025-08-28 21:50:48 +0530

    Add files via upload

---
+++ checking alphabets.py
@@ -0,0 +1,6 @@
+#checking if the character given is an alphabet or not
+character="M"
+if character.isalpha():
+    print("the given character is an alphabet")
+else:
+    print("the given character is not an alphabet")

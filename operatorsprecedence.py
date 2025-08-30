202a434ee4221b75934a7976a4ef8c22977ad83b
rayyan-ship-it <rayyanop383@gmail.com>
2025-08-30 18:39:54 +0530

    Add files via upload

---
+++ operatorsprecedence.py
@@ -0,0 +1,13 @@
+A=8
+B=7
+C=9
+D=5
+f=(A+B)*C/D
+print("the value of a+b *c/d is",f)
+#checking the alphabets
+character="codingal"
+age=5
+if character=="codingal" or character=="python" and age>2:
+    print("welcome to the world of codingal")
+else:
+    ("you are not welcome")

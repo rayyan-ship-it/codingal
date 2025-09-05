cccb87575d87d088940a65130517207d42b2c6db
rayyan-ship-it <rayyanop383@gmail.com>
2025-09-05 17:07:48 +0400

    Add files via upload

---
+++ divisiblenumber.py
@@ -0,0 +1,8 @@
+num1=int(input("enter the number"))
+num2=int(input("enter the number"))
+divisiblity=num1%num2
+if divisiblity==0:
+    print("the number is divisible by the another number")
+else:
+    print("the number is not divisible by another number")
+

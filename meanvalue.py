b583c0c00e5a2e410bad242ca6bb079eabe798c9
rayyan-ship-it <rayyanop383@gmail.com>
2025-09-07 19:38:31 +0400

    Add files via upload

---
+++ meanvalue.py
@@ -0,0 +1,10 @@
+mean1=38
+wrongnumber=36
+correctnumber=56
+totalnumber=40
+sum=mean1*totalnumber
+print("the sum of 40 number is ",sum)
+num2=sum-(wrongnumber-correctnumber)
+print(num2)
+mean2=num2/totalnumber
+print("value of mean2 is",mean2)

4c59b8d6c9625171de7d5fcdeec57616b74c5f6b
rayyan-ship-it <rayyanop383@gmail.com>
2025-09-12 22:27:09 +0400

    Add files via upload

---
+++ studentsage.py
@@ -0,0 +1,6 @@
+#students age
+studentage=int(input("enter the student age"))
+if studentage>=10 and studentage<=20:
+    print("the students are allowed")
+else:
+    print("the students are not allowed")

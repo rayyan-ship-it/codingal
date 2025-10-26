c60de56782c5567ee76e73ebfe1ba86b2cd8c395
rayyan-ship-it <rayyanop383@gmail.com>
2025-10-26 16:17:15 +0400

    Add files via upload

---
+++ circumference.py
@@ -0,0 +1,5 @@
+import math
+radius=int(input("enter the radius"))
+def circumference_circle(radius):
+    return 2* math.pi *radius
+print("the pi value for given radius is ",circumference_circle(radius))

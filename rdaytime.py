eb98287f92743d12cd3a2d64dd2eb5a613101c46
rayyan-ship-it <rayyanop383@gmail.com>
2025-11-15 17:20:41 +0400

    Add files via upload

---
+++ rdaytime.py
@@ -0,0 +1,12 @@
+import random
+import time
+def  getrandomdate(startdate,enddate):
+    print("random date between",startdate,"and",enddate)
+    randomgenerator=random.random
+    dateformat='%m/%d/%Y'
+    starttime=time.mktime(time.strptime(enddate,dateformat))
+    endtime=time.mktime(time.strptime(enddate,dateformat))
+    randomtime=starttime+randomgenerator*(endtime-starttime)
+    randomdate=time.strftime(dateformat,time.localtime(randomtime))
+    return randomdate
+print("random date=",getrandomdate("1/1/2016","12/12/2018"))

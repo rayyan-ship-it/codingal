73ed799bb0588aaacad03608588b77f20ae2f672
rayyan-ship-it <rayyanop383@gmail.com>
2025-11-15 16:36:39 +0400

    Add files via upload

---
+++ ceiling.py
@@ -0,0 +1,8 @@
+import math
+#using the ceil and floor module
+print("the floor and celing value of 23.56 are:"+str(math.ceil(23.56)+str(math.floor(23.56))))
+x=10
+y=-15
+print("the value of x after copying the sign from y is"+str(math.copysign(x,y)))
+print("the absolute value of -96 and 56 are"+str(math.fabs(-96))","+str((56)))
+print("the GCD value of 24 and 56 are:"+str(math.gcd(24,56)))

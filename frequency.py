e7f00476f226333d8a50dec515164f46b701d8ff
rayyan-ship-it <rayyanop383@gmail.com>
2025-12-27 17:16:01 +0400

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,8 @@
+test_dict={'codingal':2,'is':2,'best':2,"for":2,"coding":1}
+print("the original dictionary :"+str(test_dict))
+K=2
+res=0
+for key in test_dict:
+    if test_dict[key]==K:
+        res=res+1
+print("frequency of K is:"+str(res))

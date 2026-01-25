b63f6121406b8b2dd858cc66fc1766daa9c8456a
rayyan-ship-it <rayyanop383@gmail.com>
2026-01-25 17:12:24 +0400

    Add files via upload

---
+++ vehicle.py
@@ -0,0 +1,7 @@
+class vehicle:
+    def __init__(self,max_speed,mileage):
+     self.max_speed=max_speed
+     self.mileage=mileage
+modelx=vehicle(240,18)
+print("modelmax speed",modelx.max_speed)
+print("model mileage",modelx.mileage)

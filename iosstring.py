b4458fb93a2166f0b82e3c9d8de45a02cb4de808
rayyan-ship-it <rayyanop383@gmail.com>
2026-01-31 17:08:03 +0400

    Add files via upload

---
+++ iosstring.py
@@ -0,0 +1,11 @@
+class IOSstring():
+    def __init__(self):
+        self.str1=""
+    def get_String(self):
+        self.str1=input=("enter the string")
+    def convert_string(self):
+        print("the result is",self.str1.upper())
+str1=IOSstring()
+str1.get_String()
+str1.convert_string()
+    
